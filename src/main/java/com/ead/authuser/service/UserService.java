46bd3ed8e7e3ff715eb16113c2a705d31e170047
Emerson Lima <emersondll@outlook.com>
2023-04-07 15:29:07 -0300

    Configuração seguindo video

--- src/main/java/com/ead/authuser/service/UserService.java
+++ src/main/java/com/ead/authuser/service/UserService.java
@@ -2,6 +2,16 @@ package com.ead.authuser.service;
 
 import com.ead.authuser.model.User;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
-    User save (final User user);
+    User save(final User user);
+
+    List<User> findAll();
+
+    Optional<User> findById(final UUID id);
+
+    void delete(final User response);
 }
