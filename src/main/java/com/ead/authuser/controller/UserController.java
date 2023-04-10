806909b5f0e8871a900d0ff46c68c0795efc3aa6
Emerson Lima <emersondll@outlook.com>
2023-04-10 01:05:43 -0300

    Configuração seguindo video etapa validações, foi necessário inserir os grupos no size para aplicar a funcionalidade, acão nao apresentada n videos, e observado que o validation email tem um regex muito amplo sendo necessário criar personalizado no futuro

--- src/main/java/com/ead/authuser/controller/UserController.java
+++ src/main/java/com/ead/authuser/controller/UserController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -54,13 +55,14 @@ public class UserController {
 
     @PutMapping("/{userId}")
     public ResponseEntity<Object> updateById(@PathVariable(value = "userId") UUID userId,
-                                             @RequestBody @JsonView(UserDto.UserView.UserPut.class) UserDto userDto) {
+                                             @RequestBody @Validated(UserDto.UserView.UserPut.class)
+                                             @JsonView(UserDto.UserView.UserPut.class) UserDto userDto) {
         Optional<UserModel> response = service.findById(userId);
         if (response.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
         }
         UserModel userModel = response.get();
-            userModel.setCpf(userDto.getCpf());
+        userModel.setCpf(userDto.getCpf());
         userModel.setFullName(userDto.getFullName());
         userModel.setPhoneNumber(userDto.getPhoneNumber());
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
@@ -72,7 +74,8 @@ public class UserController {
 
     @PutMapping("/{userId}/password")
     public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
-                                                 @RequestBody @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto) {
+                                                 @RequestBody @Validated(UserDto.UserView.PasswordPut.class)
+                                                 @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto) {
         Optional<UserModel> response = service.findById(userId);
         if (response.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
@@ -91,7 +94,8 @@ public class UserController {
 
     @PutMapping("/{userId}/image")
     public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
-                                              @RequestBody @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
+                                              @RequestBody @Validated(UserDto.UserView.ImagePut.class)
+                                              @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
         Optional<UserModel> response = service.findById(userId);
         if (response.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
