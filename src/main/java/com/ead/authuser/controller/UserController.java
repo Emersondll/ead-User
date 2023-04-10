4c4fb1074e9af21e2d8eb1b30c838a87f86620c9
Emerson Lima <emersondll@outlook.com>
2023-04-10 00:08:33 -0300

    Configuração seguindo video etapa de views

--- src/main/java/com/ead/authuser/controller/UserController.java
+++ src/main/java/com/ead/authuser/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.controller;
 
+import com.ead.authuser.dto.UserDto;
 import com.ead.authuser.model.UserModel;
 import com.ead.authuser.service.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,26 +30,79 @@ public class UserController {
 
     }
 
-    @GetMapping("/id")
-    public ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
-
-        Optional<UserModel> response = service.findById(id);
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> findById(@PathVariable(value = "userId") UUID userId) {
+        Optional<UserModel> response = service.findById(userId);
         return response.<ResponseEntity<Object>>map(user -> ResponseEntity.status(HttpStatus.OK).body(user))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found"));
 
 
     }
 
-    @DeleteMapping("/id")
-    public ResponseEntity<Object> deleteById(@PathVariable(value = "id") UUID id) {
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteById(@PathVariable(value = "userId") UUID userId) {
 
-        Optional<UserModel> response = service.findById(id);
+        Optional<UserModel> response = service.findById(userId);
 
         if (response.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
         }
         service.delete(response.get());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User Deleted");
+
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateById(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserDto.UserView.UserPut.class) UserDto userDto) {
+        Optional<UserModel> response = service.findById(userId);
+        if (response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
+        }
+        UserModel userModel = response.get();
+            userModel.setCpf(userDto.getCpf());
+        userModel.setFullName(userDto.getFullName());
+        userModel.setPhoneNumber(userDto.getPhoneNumber());
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        service.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userModel);
+
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
+                                                 @RequestBody @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto) {
+        Optional<UserModel> response = service.findById(userId);
+        if (response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
+        }
+        if (!response.get().getPassword().equals(userDto.getOldPassword())) { // TODO Melhorar validação
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Mismatched Password");
+        }
+        UserModel userModel = response.get();
+        userModel.setPassword(userDto.getPassword());
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        service.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Password Updated Successfully");
+
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
+                                              @RequestBody @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
+        Optional<UserModel> response = service.findById(userId);
+        if (response.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found");
+        }
+
+        UserModel userModel = response.get();
+        userModel.setImageUrl(userDto.getImageUrl());
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        service.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userModel);
 
     }
 }
--- src/main/java/com/ead/authuser/dto/UserDto.java
+++ src/main/java/com/ead/authuser/dto/UserDto.java
@@ -1,17 +1,45 @@
 package com.ead.authuser.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
+
+    public interface UserView {
+
+        public static interface RegistrationPost {
+        }
+
+        public static interface UserPut {
+        }
+
+        public static interface PasswordPut {
+        }
+
+        public static interface ImagePut {
+        }
+    }
+
+    private UUID id;
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+    @JsonView({UserView.ImagePut.class})
     private String imageUrl;
 }
